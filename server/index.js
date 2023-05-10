e7671227acd0c5712598f1337c8253fb9268283b
salmansarwarr <muhammadsalmansarwar32@gmail.com>
2023-05-10 22:46:51 +0500

    add cors headers

--- server/index.js
+++ server/index.js
@@ -9,9 +9,14 @@ import { connectDB } from './config/db.js';
 const app = express();
 dotenv.config();
 
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use(cors());
 
 app.use('/posts', postRoutes);
